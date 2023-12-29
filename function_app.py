1b9e4bf9b59eb680b23968417c0a98ba6d792095
Lucas Climenhage <luke.climenhage@gmail.com>
2023-12-29 17:08:32 -0500

    Update references in function_app

--- function_app.py
+++ function_app.py
@@ -1,11 +1,11 @@
 import azure.functions as func
 import logging
-from .email import _send_email, _create_acknowledgement_email, _create_internal_email
+from Email import _send_email, _create_acknowledgement_email, _create_internal_email
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="contact_form")
-async def contact_form(req: func.HttpRequest) -> func.HttpResponse:
+def contact_form(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     try:
         req_body = req.get_json()
@@ -20,8 +20,8 @@ async def contact_form(req: func.HttpRequest) -> func.HttpResponse:
     try:
         internal_email_copy = _create_internal_email(name, email, message)
         external_email_copy = _create_acknowledgement_email(name, email, message)
-        await _send_email(internal_email_copy)
-        await _send_email(external_email_copy)
+        _send_email(internal_email_copy)
+        _send_email(external_email_copy)
         return func.HttpResponse(
             "Successfully submitted contact form.",
             status_code=200
@@ -32,13 +32,3 @@ async def contact_form(req: func.HttpRequest) -> func.HttpResponse:
             "Error while submitting form.",
             status_code=500
         )
-
-
-
-    # if name:
-    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    # else:
-    #     return func.HttpResponse(
-    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-    #          status_code=200
-    #     )
