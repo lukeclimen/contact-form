9bb143911a43b393fe18ced9f72876f5df9fa1e6
Lucas Climenhage <luke.climenhage@gmail.com>
2023-12-29 13:23:29 -0500

    Create email file for creating and sending emails

---
+++ email.py
@@ -0,0 +1,13 @@
+def _send_email(name: str, email: str, message: str) -> None:
+    """Sends an email using Azure Communication Services"""
+    pass
+
+
+def _create_internal_email(name: str, email: str, message: str) -> str:
+    """Crafts the email copy for the internally-sent email"""
+    pass
+
+
+def _create_acknowledgement_email(name: str, email: str, message: str) -> str:
+    """Crafts the acknowledgement email sent to the person who submitted the form"""
+    pass
